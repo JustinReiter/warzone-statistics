aa384c9934989416c890795e795d0bde4e646b8d
Justin Reiter <JDReiter@uwaterloo.ca>
2020-10-29 00:07:44 -0400

    Create skeleton for navbar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MainMenu from './MainMenu';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <MainMenu />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
