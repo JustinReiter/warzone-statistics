737d5ca0dd49e5716686ee9292c41bbf3261e2dc
Justin Reiter <JDReiter@uwaterloo.ca>
2020-11-02 22:48:49 -0500

    Add function to create clean database initial game input

---
+++ database-setup/cleanDataToSQL.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const ladders = ["Seasonal XLI", "Seasonal XLII"];
+const ladderIds = [4075, 4076];
+
+// Returns list of file names
+function GetAllGameFiles() {
+    let res = fs.readdirSync("./GameData");
+
+    return res;
+}
+
+function writeSQLToFile(games) {
+    let logger = fs.createWriteStream('init_game_data.sql');
+    let hasSeenSecondLadder = false;
+
+    logger.write(`-- Insert ${ladders[0]} games to database\n`);
+    for (const game of games) {
+        if (!hasSeenSecondLadder && game.lid == 4076) {
+            logger.write(`\n-- Insert ${ladders[1]} games to database\n`);
+            hasSeenSecondLadder = true;
+        }
+
+        logger.write(`INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (${game.gid}, ${game.lid}, ${game.winner}, ${game.booted}, ${game.turns}, '${game.start_date}', '${game.end_date}', ${game.player0_id}, '${game.player0_colour}', ${game.player1_id}, '${game.player1_colour}');\n`);
+    }
+
+    logger.close();
+}
+
+function ReadGameFiles() {
+    let dataToOutput = [];
+    for (let i = 0; i < ladders.length; i++) {
+        let games = fs.readdirSync("./GameData/" + ladders[i]);
+
+        let count = 0;
+        for (const game of games) {
+            if (count == 5) {
+                break;
+            }
+            count++;
+            let gameData = JSON.parse(fs.readFileSync("./GameData/" + ladders[i] + "/" + game));
+
+            dataToOutput.push({
+                gid: gameData.id,
+                lid: ladderIds[i],
+                winner: Number(gameData.players[0].state === "Won"),
+                booted: Number(gameData.players.filter((player) => player.state === "Booted").length),
+                turns: gameData.numberOfTurns,
+                // start_date: new Date(gameData.startDate).toISOString().slice(0, 19).replace('T', ' ');,
+                end_date: new Date(gameData.lastTurnTime).toISOString().slice(0, 19).replace('T', ' '),
+                player0_id: Number(gameData.players[0].id),
+                player0_colour: gameData.players[0].color,
+                player1_id: Number(gameData.players[1].id),
+                player1_color: gameData.players[1].color
+            });
+        }
+    }
+
+    writeSQLToFile(dataToOutput);
+}
+
+ReadGameFiles();
---
+++ database-setup/init_game_data.sql
@@ -0,0 +1,13 @@
+-- Insert Seasonal XLI games to database
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (23343682, 4075, 0, 1, -1, 'undefined', '2020-07-29 04:27:34', 6862295334, '#0000ff', 2723578065, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (23343683, 4075, 0, 0, 5, 'undefined', '2020-07-31 08:16:13', 9272651105, '#0000ff', 2347481119, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (23343684, 4075, 1, 0, 6, 'undefined', '2020-08-03 19:39:24', 3826046922, '#606060', 1765340859, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (23343685, 4075, 1, 0, 8, 'undefined', '2020-08-04 20:10:51', 8285645937, '#23a0ff', 7617058940, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (23343686, 4075, 1, 0, 10, 'undefined', '2020-08-12 13:35:52', 5921008590, '#606060', 5613510227, 'undefined');
+
+-- Insert Seasonal XLII games to database
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (24340598, 4076, 0, 0, 8, 'undefined', '2020-10-28 15:27:20', 4430152490, '#606060', 3290801210, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (24340600, 4076, 1, 0, 9, 'undefined', '2020-10-22 09:08:54', 5966041018, '#ffaf56', 81101893884, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (24340603, 4076, 0, 0, 5, 'undefined', '2020-10-23 12:12:27', 754501262, '#00ff05', 889757420, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (24340604, 4076, 1, 1, 3, 'undefined', '2020-11-02 02:48:20', 7296500493, '#008000', 32116011401, 'undefined');
+INSERT INTO games (gid, lid, winner, booted, turns, start_date, end_date, player0_id, player0_colour, player1_id, player1_colour) VALUES (24340605, 4076, 0, 0, 8, 'undefined', '2020-10-27 22:15:24', 22107763408, '#23a0ff', 64121290289, 'undefined');
