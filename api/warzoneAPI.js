4602f381b67584c6a79d9a68755844257b21e1ba
Justin Reiter <JDReiter@uwaterloo.ca>
2020-11-04 19:24:14 -0500

    Fix body data in warzone api fetch

--- api/warzoneAPI.js
+++ api/warzoneAPI.js
@@ -3,8 +3,7 @@ const request = require('sync-request');
 // Return ladder game IDs to find new games
 function getLadderGameIDs(ladderid) {
     return JSON.parse(request('POST', process.env.WZ_BASE_URL + "/GameIDFeed?LadderID=" + ladderid, {
-                    Email: encodeURIComponent(process.env.WZ_EMAIL),
-                    APIToken: encodeURIComponent(process.env.WZ_TOKEN)
+                    body: encodeURI(`Email=${process.env.WZ_EMAIL}&APIToken=${process.env.WZ_TOKEN}`)
                 }).getBody('utf-8')).gameIDs;
 }
 
