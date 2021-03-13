5e63225c4c35c4bd76c6a16bc7c88a3bd92c1100
Justin Reiter <JDReiter@uwaterloo.ca>
2021-03-13 17:10:20 -0500

    Add ssl flag for heroku database

--- api/database.js
+++ api/database.js
@@ -1,4 +1,4 @@
 var pgp = require('pg-promise')();
-var db = pgp(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`);
+var db = pgp(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?ssl=true`);
 
 module.exports = db;
