2417e54f7fea548d2ec4963a57eb9043ebb57b83
Justin Reiter <JDReiter@uwaterloo.ca>
2021-03-13 17:20:19 -0500

    Reduce number of connections to 1

--- api/database.js
+++ api/database.js
@@ -2,7 +2,7 @@ var pgp = require('pg-promise')();
 
 var config = {
     connectionString: `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-    max: 20,
+    max: 1,
     ssl:{rejectUnauthorized: false}
 }
 var db = pgp(config);
